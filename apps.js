8514d4307434dde85e24a3c706605a8b5ca73488
gabriel1314-tech <gabriel.7344@colegioprogresso.g12.br>
2026-05-25 12:13:18 -0300

    fazendo

--- apps.js
+++ apps.js
@@ -4,6 +4,12 @@ const display = document.getElementById('display')
 
 function inserirTarefa(){
     let tarefa = input.value
+    
+    if(tarefa == ''){
+        alert('favor inserir uma tarefa')
+        return
+    }
+
     display.innerHTML += 
     `<div class="tarefa">
         ${tarefa}
@@ -15,4 +21,11 @@ function inserirTarefa(){
     input.value = ''
 }
 
+function removerTarefa(event){
+    if(event.target.classList.contains('delete')){
+        event.target.parentElement.parentElement.remove()
+    }
+}
+
 button.addEventListener("click", inserirTarefa)
+display.addEventListener("click", removerTarefa)
